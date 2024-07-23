a972b3f687979c107951b9d2e91deffb4e4a5441
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-07-23 11:13:53 -0300

    initial commit

--- MeshGradientiOs18/ContentView.swift
+++ MeshGradientiOs18/ContentView.swift
@@ -3,19 +3,28 @@
 //  MeshGradientiOs18
 //
 //  Created by Bruno Oliveira on 23/07/24.
-//
+// Inspiration: https://x.com/ios_dev_alb/status/1815352371393839592/photo/1
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [
+                [0.0, 0.0], [0.5, 0.0], [1.0, 0.0],
+                [0.5, 1.0], [0.7, 0.5], [1.0, 0.7],
+                [0.0, 1.0], [0.0, 0.5], [0.0, 0.5]
+            ],
+            colors:[
+                .teal, .purple, .indigo,
+                .purple, .blue, .pink,
+                .purple, .red, .purple
+            ]
+        )
+        .ignoresSafeArea()
+        .shadow(color: .gray, radius: 25, x: -10, y: 10)
     }
 }
 
