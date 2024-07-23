b5df13b2c323b0e3824b760a47c57f5c6c72dc14
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-07-23 10:51:33 -0300

    Initial Commit

---
+++ MeshGradientiOs18/MeshGradientiOs18App.swift
@@ -0,0 +1,17 @@
+//
+//  MeshGradientiOs18App.swift
+//  MeshGradientiOs18
+//
+//  Created by Bruno Oliveira on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MeshGradientiOs18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
